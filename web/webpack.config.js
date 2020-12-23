577e864c065a3d2dc2e498a6175612ce6a0fb596
kathleenfrench <kfrench09@gmail.com>
2020-12-23 16:55:54 -0600

    rework webpack build so all served files go to a 'dist' directory

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
-const _distDir = path.resolve(__dirname, '../static/assets/js');
+const _staticDir = path.resolve(__dirname, '../static');
 const _crateDir = path.resolve(__dirname, '../pterm');
 const _crateOut = path.resolve(__dirname, 'pkg')
 const _rootDir = path.resolve(__dirname, 'src');
+const _dist = path.resolve(__dirname, "../dist");
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   // see: https://github.com/webpack/webpack/issues/6615
   entry : `${_rootDir}/main.js`,
   output : {
-    path : `${_distDir}`,
+    path : `${_dist}/assets/js`,
     filename : "bundle.js",
   },
   mode : "development",
@@ -18,10 +21,16 @@ module.exports = {
     asyncWebAssembly: true,
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: _staticDir, to: `${_dist}`, ignore: ['*.css'] },
+      { from: `${_staticDir}/css`, to: `${_dist}/assets/css` },
+    ]),
     new WasmPackPlugin({ 
       crateDirectory: _crateDir,
       outDir: _crateOut,
       extraArgs: "--no-typescript",
+      forceMode: 'release',
     }),
   ]
 };
