887958d0b7dd595b2655282dfb1f585e375cba37
kathleenfrench <kfrench09@gmail.com>
2020-12-26 15:34:52 -0600

    set port in the settings

--- src/settings.rs
+++ src/settings.rs
@@ -51,6 +51,9 @@ impl Settings {
         // and env var prefixed with 'KF_' will override any previously set value
         s.merge(Environment::with_prefix("kf").separator("_"))?;
 
+        let port = std::env::var("PORT").unwrap_or_else(|_| "3000".into());
+        s.set("server.port", port)?;
+
         s.try_into()
     }
 }
